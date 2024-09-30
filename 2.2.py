454379f75888293692e0727aa03c341344513fb4
MatveiRomanov <rivernate150@gmail.com>
2024-09-30 11:58:55 +0900

    Add files via upload

--- 2.2.py
+++ 2.2.py
@@ -3,10 +3,10 @@ def main():
     a = input("Первая строка: ")
     b = input("Вторая строка: ")
 
-    if a in "ДА" and b in "ДА":
+    if a == "ДА" and b == "ДА":
         print("ВЕРНО")
 
-    elif a in "НЕТ" and b in "НЕТ":
+    elif a == "НЕТ" and b == "НЕТ":
         print("ВЕРНО")
 
     else:
