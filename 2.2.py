4b348ceab1ac2dc9157eebd1f5bcd50eff2fb7ad
MatveiRomanov <rivernate150@gmail.com>
2024-09-30 11:45:29 +0900

    Add files via upload

--- 2.2.py
+++ 2.2.py
@@ -1,5 +1,15 @@
 def main():
 
+    a = input("Первая строка: ")
+    b = input("Вторая строка: ")
 
+    if a in "ДА" and b in "ДА":
+        print("ВЕРНО")
+
+    elif a in "НЕТ" and b in "НЕТ":
+        print("ВЕРНО")
+
+    else:
+        print("НЕВЕРНО")
 
 main()
