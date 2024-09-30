9e4e6c641d89cb93c13f8abb759779c4947e7242
MatveiRomanov <rivernate150@gmail.com>
2024-09-30 12:13:46 +0900

    Add files via upload

---
+++ 2.11.py
@@ -0,0 +1,11 @@
+def main():
+
+    num = int(input("Введите числло: "))
+
+    if num % 2 == 0:
+        print(f"Число", num ,"четное")
+
+    else:
+        print(f"Число", num, "нечетное")
+
+main()
