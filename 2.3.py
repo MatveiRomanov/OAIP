abfe5fe06c53697de31b0833611790eda440cc6e
MatveiRomanov <rivernate150@gmail.com>
2024-10-03 12:14:34 +0900

    Add files via upload

---
+++ 2.3.py
@@ -0,0 +1,18 @@
+def main():
+
+    one = input()
+    two = input()
+    three = input()
+
+    for i in range(1):
+
+        if one == 'раз' and two == 'два' and three == 'три':
+            print('ГОРИ')
+
+        elif one == '1' and two == '2' and three == '3':
+            print('ГОРИ')
+
+        else:
+            print('НЕ ГОРИ')
+
+main()
