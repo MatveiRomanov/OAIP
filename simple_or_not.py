10c7f2a091d0f58d600f86f0ee274edf1cadde5a
MatveiRomanov <rivernate150@gmail.com>
2025-01-31 21:23:40 +0900

    Update simple_or_not.py

--- simple_or_not.py
+++ simple_or_not.py
@@ -1,2 +1,10 @@
-def SON():
-    1
+def SON(n, divisor=None):
+    if n <= 1:
+        return False
+    if divisor is None:
+        divisor = n - 1
+    if divisor == 1:
+        return True
+    if n % divisor == 0:
+        return False
+    return SON(n, divisor - 1)
