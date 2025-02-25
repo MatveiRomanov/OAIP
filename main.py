2c9c622da835503e64eb2a21e80a842fe2094b95
MatveiRomanov <rivernate150@gmail.com>
2025-02-25 10:40:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from chest import Chest
 from button import Button
+from tamagochi import Tamagochi
 
 chest = Chest(["Кристал", "Чары усиления", "Улучшение брони"])
 chest.lock_out()
 chest.open()
 chest.close()
 chest.lock_up()
-
 
 
 button = Button("Пока пока", color="красный")
@@ -17,3 +17,16 @@ button.re_rext("Привет")
 button.re_size("Маленький")
 
 button.button_info()
+
+
+tamagochi = Tamagochi("ГГ")
+tamagochi.play()
+tamagochi.play()
+tamagochi.play()
+tamagochi.play()
+tamagochi.play()
+tamagochi.feed()
+tamagochi.sleep()
+tamagochi.nuzhda()
+tamagochi.work()
+tamagochi.status()
