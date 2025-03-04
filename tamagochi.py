d1ff0d44bde8fdd5f8b91672ef49e3ea0259847d
MatveiRomanov <rivernate150@gmail.com>
2025-03-04 11:00:08 +0900

    Update tamagochi.py

--- tamagochi.py
+++ tamagochi.py
@@ -5,6 +5,7 @@ class Tamagochi:
         self.happiness = 100
         self.energy = 100
         self.bathroom = 0
+        self.alive = True
 
     def status(self):
         print(
@@ -14,67 +15,61 @@ class Tamagochi:
             f"Бодрость: {self.energy}\n "
             f"Нужда: {self.bathroom}")
 
+    def death(self):
+        if self.hunger <= 0 or self.energy <= 0 or self.happiness < 0 or self.bathroom >= 100:
+            self.alive = False
+        return not self.alive
+
     def feed(self):
-        if self.hunger < 100:
+        if self.death():
+            print(f"{self.name} мертв(а)")
+
+        elif self.death == False and self.hunger < 100:
             self.hunger = min(100, self.hunger + 30)
             self.bathroom += 20
             self.energy += 5
             self.happiness = min(100, self.happiness + 5)
-            print(
-                f"{self.name} поел(а).\n "
-                f"Сытость = {self.hunger}\n "
-                f"Нужда = {self.bathroom}\n "
-                f"Бодрость = {self.energy}\n "
-                f"Счастье = {self.happiness}")
+            self.status()
         else:
             print(f"{self.name} не голод(ен/на)")
 
     def play(self):
-        if self.energy > 20:
+        if self.death():
+            print(f"{self.name} мертв(а)")
+        elif self.energy > 20:
             self.happiness = min(100, self.happiness + 20)
             self.energy -= 20
             self.bathroom += 10
             self.hunger -= 10
-            print(
-                f"{self.name} поиграл(а)\n "
-                f"Счастье = {self.happiness}\n "
-                f"Бодрость = {self.energy}\n "
-                f"Нужда = {self.bathroom}\n "
-                f"Сытость =  {self.hunger}")
+            self.status()
         else:
             print(f"{self.name} устал(а)")
 
     def sleep(self):
-        if self.energy < 100:
+        if self.death():
+            print(f"{self.name} мертв(а)")
+        elif self.energy < 100:
             self.energy = min(100, self.energy + 30)
             self.hunger -= 30
             self.happiness = min(100, self.happiness + 5)
             self.bathroom += 10
-            print(
-                f"{self.name} поспал(а)\n "
-                f"Бодрость = {self.energy}\n "
-                f"Счастье = {self.happiness}\n "
-                f"Нужда = {self.bathroom}\n "
-                f"Сытость =  {self.hunger}")
+            self.status()
         else:
             print(f"{self.name} не хочет спать")
 
-    def nuzhda(self):
-        if self.bathroom > 0:
+    def need(self):
+        if self.death():
+            print(f"{self.name} мертв(а)")
+        elif self.bathroom > 0:
             self.bathroom = min(100, self.bathroom - self.bathroom)
-            print(f"{self.name} сходил(а) в туалет\n"
-                  f"Нужда = {self.bathroom}")
-
+            self.status()
         else:
             print(f"{self.name} не испытывает нужду")
 
     def work(self):
-        self.happiness -= self.happiness
-        self.energy -= self.energy
-        print(f"{self.name} поработал(а) в Осетинских пирогах. Устал и несчастен")
-        
-
-
-    def sleep(self):
-        if self.energy < 100:
-            self.energy = min(100, self.energy + 30)
+        if self.death():
+            print(f"{self.name} мертв(а)")
+        elif self.alive:
+            self.happiness = min(100, self.happiness - 50)
+            self.energy = min(100, self.energy - 50)
+            print(f"{self.name} поработал(а) в Осетинских пирогах. Устал и несчастен")
