f382f730e5a2080b86191e98fe156907d6763c95
MatveiRomanov <rivernate150@gmail.com>
2025-03-11 14:23:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ chest.close()
 chest.lock_up()
 
 
-button = Button("Пока пока", x = 1, y = 1, z = 1, color="красный")
+button = Button("Пока пока", x = 1, y = 1, a = 1, b = 1, color="красный")
 button.press()
 
 button.re_color("черный")
 button.re_rext("Привет")
-print(button.re_size(new_x = 15, new_y = 20, new_z = 10))
+print(button.re_size(new_x = 15, new_y = 20, new_a = 10, new_b = 10))
 
 button.button_info()
 
