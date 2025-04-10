8a2cfcc69959645b964aac3c9d6133e00a63393a
MatveiRomanov <rivernate150@gmail.com>
2025-04-10 10:56:16 +0900

    Delete 1.1.py

--- 1.1.py
+++
@@ -1,8 +0,0 @@
-def main():
-    nFilm = input("Название фильма:")
-    nkinoteatr = input("Кинотеатр:")
-    time = input("Время:")
-
-    print(f"Билет на", nFilm, "в", nkinoteatr ,"на", time, "забронирован")
-
-main()
